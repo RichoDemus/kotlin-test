9cee3be2e34f99f566bfe7a1b7e223f93ddf631b
Richard Tjerngren <richard.tjerngren@netent.com>
2016-04-29 16:37:53 +0200

    Playing with higher order functions

---
+++ src/main/kotlin/demo/higher_order_function/HigherOrderFunction.kt
@@ -0,0 +1,28 @@
+package demo.higher_order_function
+
+import kotlin.reflect.KFunction1
+
+
+fun main(args: Array<String>) {
+    val id = 10
+    val dao = { it: Int -> mapOf(Pair(80, 8080), Pair(443, 8443)).get(it) }
+    val port = 80
+
+    idToContainer(id)
+
+}
+
+
+//fun idToContainer(dao: KFunction1<Int, Container>):(Int) -> Container {
+fun idToContainer(dao: KFunction1<Id, Container>):(Int) -> Container {
+
+    return {id:Int -> dao(id)}
+}
+
+fun idToContainer(id:Int) = Id(id)
+fun containerDao(id:Id) = Container(id)
+
+
+data class Container(val id:Id)
+
+data class Id(val id:Int)
